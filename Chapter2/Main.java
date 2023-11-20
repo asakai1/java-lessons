3498538f16a9c833b408e36388ee9ca7b4a166cb
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-11-20 12:21:41 +0000

    Add Chapter2-4

--- Chapter2/Main.java
+++ Chapter2/Main.java
@@ -34,3 +34,41 @@ public class Main {
   }
 }
 
+//エスケープシーケンスを使用した場合
+public class Main {
+  public static void main(String[] args){
+    System.out.println("私の好きな記号は二重引用符（¥"）です");
+  }
+}
+
+/*
+式の評価(evaluation)
+結合規則...演算子ごとに決められた方向
+・算術演算子
+＋,-,*,/,%...左から右
+・文字列結合演算子
++...左から右
+・代入演算子（代入は基本的に最後）
+=,+=,-=,*=,/=,%=,+=...右から左
+・インクリメント/デクリメント演算子(優先順位最高)
+++,--...左から右
+*/
+
+public class Main {
+  public static void main(String[] args){
+    int a;
+    a = 100;
+    a++; //aの内容が１増える
+    System.out.println(a);
+  }
+}
+
+//++,--を他の演算子を一緒に使うとエラーの原因になる
+public class Main {
+  public static void main(String[] args){
+    int a = 10;
+    int b = 10;
+    System.out.println(++a + 50);
+    System.out.println(b++ + 50);
+  }
+}
